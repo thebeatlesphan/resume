e2ac09e23e78dfc07dc8dfb31177584cba989f64
andy <aphanIT@protonmail.com>
2022-03-12 17:31:39 -0500

    added functionality to projects

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-// //navigation menu
-// function openNav() {
-//     document.getElementById("overlay").style.height = "100%";
-// }
-
-// function closeNav() {
-//     document.getElementById("overlay").style.height = "0%";
-// }
-
-// //modal for projects
-// const modal = document.getElementsByClassName("modals")
-
+//color scheme
 function changeTheme() {
   const theme = document.querySelector("*");
-  const background = document.querySelector(".stars")
-  const smiley = document.querySelector(".smiley")
-  console.log(background)
+  const background = document.querySelector(".stars");
+  const smiley = document.querySelector(".smiley");
+
   theme.classList.toggle("dark");
-  background.classList.toggle("dark")
-  smiley.classList.toggle("dark")
-  console.log("theme");
+  background.classList.toggle("dark");
+  smiley.classList.toggle("dark");
+}
+
+//mouseover projects
+const display = document.getElementsByClassName("display");
+const projects = display[0].children;
+
+function onHover(number) {
+  const snippet = projects[number].children[0]
+  const toProject = projects[number].children[1].children[2]
+  
+  toProject.style.textDecoration = "underline"
+  snippet.style.transform = "scale(1.03)"
+}
+
+function offHover(number) {
+  const snippet = projects[number].children[0]
+  const toProject = projects[number].children[1].children[2]
+  
+  toProject.style.textDecoration = "none"
+  snippet.style.transform = "scale(1)"
 }
