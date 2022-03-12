c014f6d7f552fac815341e134fd6fca824bea737
andy <aphanIT@protonmail.com>
2022-03-12 02:01:18 -0500

    dark mode

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-//navigation menu
-function openNav() {
-    document.getElementById("overlay").style.height = "100%";
-}
+// //navigation menu
+// function openNav() {
+//     document.getElementById("overlay").style.height = "100%";
+// }
 
-function closeNav() {
-    document.getElementById("overlay").style.height = "0%";
-}
+// function closeNav() {
+//     document.getElementById("overlay").style.height = "0%";
+// }
 
-//modal for projects
-const modal = document.getElementsByClassName("modals")
+// //modal for projects
+// const modal = document.getElementsByClassName("modals")
+
+function changeTheme() {
+  var theme = document.querySelector("*");
+  theme.classList.toggle("dark");
+  console.log("theme");
+}
