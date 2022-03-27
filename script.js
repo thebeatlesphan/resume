31d24be71e732d7f96b278544fad9ce3932b8f9c
andy <aphanIT@protonmail.com>
2022-03-26 22:37:07 -0400

    js modal complete

--- script.js
+++ script.js
@@ -14,34 +14,34 @@ const display = document.getElementsByClassName("display");
 const projects = display[0].children;
 
 function onHover(number) {
-  const snippet = projects[number].children[0]
-  const toProject = projects[number].children[1].children[2]
-  
-  toProject.style.textDecoration = "underline"
-  snippet.style.transform = "scale(1.03)"
+  const snippet = projects[number].children[0];
+  const toProject = projects[number].children[1].children[2];
+
+  toProject.style.textDecoration = "underline";
+  snippet.style.transform = "scale(1.03)";
 }
 
 function offHover(number) {
-  const snippet = projects[number].children[0]
-  const toProject = projects[number].children[1].children[2]
-  
-  toProject.style.textDecoration = "none"
-  snippet.style.transform = "scale(1)"
+  const snippet = projects[number].children[0];
+  const toProject = projects[number].children[1].children[2];
+
+  toProject.style.textDecoration = "none";
+  snippet.style.transform = "scale(1)";
 }
 
 //modal
-const modalParent = document.getElementsByClassName("modal")
-const modal = modalParent[0].children
-console.log(modalParent[0])
+const modalParent = document.getElementsByClassName("modal");
+const modal = modalParent[0].children;
 
 function showModal(number) {
-  modalParent[0].style.display = "flex"
+  modalParent[0].style.display = "flex";
   modal[number].style.display = "block";
-  console.log("hiii")
 }
 
 window.onclick = function(e) {
   if (e.target == modalParent[0]) {
-    modalParent[0].style.display = "none"
-  } 
-}
+    modalParent[0].style.display = "none";
+    modal[0].style.display = "none";
+    modal[1].style.display = "none";
+  }
+};
